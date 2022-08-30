26566930e1ab8a9ccb49969a1f172261ebc37982
spiricom <jeff@snyderphonics.com>
2022-08-29 20:45:02 -0400

    removed old zip file

--- PSOC5_electrobass_stringsensors/CE210514_PSOC3_5_Thermistor.cydsn/main.c
+++ PSOC5_electrobass_stringsensors/CE210514_PSOC3_5_Thermistor.cydsn/main.c
@@ -345,6 +345,7 @@ volatile float testVal = 0.0f;
 
 
 uint32_t SPI_errors = 0;
+/*
 CY_ISR(SleepIsr_function)
 {
     if (USB_active)
@@ -363,6 +364,7 @@ CY_ISR(SleepIsr_function)
     //Clear pending interrupt
     SleepTimer_GetStatus();
 }
+*/
 CY_ISR(Vbus_function)
 {
     //plug or unplug event
@@ -893,86 +895,24 @@ void USB_service(void)
          /* Reinitialize after SET_CONFIGURATION or SET_INTERFACE Requests */
         if(USB_IsConfigurationChanged() != 0x00)
         {
-            //USB_LoadInEP(USB_midi_in_ep, USB_midiInBuffer,(uint16) USB_midiInPointer);
-            USB_LoadInEP(USB_midi_in_ep, USB_midiInBuffer, 64);
+            USB_LoadInEP(USB_midi_in_ep, USB_midiInBuffer,(uint16) USB_midiInPointer);
+            //USB_LoadInEP(USB_midi_in_ep, USB_midiInBuffer, 64);
             USB_ReadOutEP(USB_midi_out_ep,USB_midiOutBuffer, 64);
                                              /* configuring the DMAs for the first time only, the data as soon as received
                                              in the EP buffer is Transferred by DMA to the buffer as defined here with no CPU intervention */
             USB_EnableOutEP(USB_midi_out_ep);       /* Note 3.*/
         }
-
-         /* Check that all data has been transfered and IN Buffer is empty */
-         if (USB_GetEPState(USB_midi_in_ep) == USB_IN_BUFFER_EMPTY)
-         {
-            /*Rearm the IN Endpoint (EP1) */
-            USB_LoadInEP(USB_midi_in_ep, USB_NULL, (uint16) USB_midiInPointer);
-         }
-             
-         if (USB_GetEPState(USB_midi_out_ep) == USB_OUT_BUFFER_FULL)
-         {
-            USB_EnableOutEP(USB_midi_out_ep);       /* Note 3.*/
-         }
- #if 0
-        if(USBFS_IsConfigurationChanged())   /* Will be entered the first time a setconfiguration is received. 
-                                               Note : 2 */
+        if ((USB_active) && (USB_VBusPresent()))
         {
-            USBFS_LoadInEP(IN_EP,&BufferIn[0],MAX_BUF);
-            USBFS_ReadOutEP(OUT_EP,&BufferOut[0],MAX_BUF);
-                                             /* configuring the DMAs for the first time only, the data as soon as received
-                                             in the EP buffer is Transferred by DMA to the buffer as defined here with no CPU intervention */
-            USBFS_EnableOutEP(OUT_EP);       /* Note 3.
-                                
+            USB_MIDI_IN_Service();
         }
-        
-        /* If content is there in Out endpoint,dump the content and enable the endpoint again.*/
-        if(USBFS_GetEPState(OUT_EP)==USBFS_OUT_BUFFER_FULL)
-        {   
-            //USBFS_ReadOutEP(OUT_EP,&Buffernew[0],MAX_BUF);  /* Use this API again, to initialize the DMAs again to point to a new buffer
-                                                              /* where the data should be stored.can now read the data available in the 
-                                                              buffer - BufferOut */
-            USBFS_EnableOutEP(OUT_EP);
-        }
-        /* Maximum throughput for an out endpoint is 600-700 KB/sec - When you are not reading the endpoint, Simply arming it. (128pxfer - 64xfers/queue)*/ 
-
-        /* If In endpoint is empty, fill it with data*/
-        if(USBFS_GetEPState(IN_EP)== USBFS_IN_BUFFER_EMPTY)
-        {   
-            //USBFS_LoadInEP(IN_EP,&BufferIn[0],MAX_BUF);     /* Use this API again to point to a new memory from where data should be */
-                                                              /*   copied and stored. */
-            /* The DMAs are already configured to point to the buffer. Simply pass a null argument to send the data to the endpoint. As 
-            soon as the Host issues a request, a Packet is received.*/
-            USBFS_LoadInEP(IN_EP,NULL,MAX_BUF);               /* Note 4, Note 5. */
-        }
-#endif
-            if ((USB_active) && (USB_VBusPresent()))
-            {
-                USB_MIDI_IN_Service();
-            }
 
 
 
-            if ((USB_active) && (USB_VBusPresent()))
-            {
-                USB_MIDI_OUT_Service();
-            }
-
-/*
-        if( usbActivityCounter >= 2u ) 
+        if ((USB_active) && (USB_VBusPresent()))
         {
-
-            USB_Suspend();
-
-            CyPmSaveClocks();
-            CyPmSleep(PM_SLEEP_TIME_NONE, PM_SLEEP_SRC_PICU);
-            CyPmRestoreClocks();
-
-            USB_Resume();
-            USB_MIDI_EP_Init();
-            
-            usbActivityCounter = 0u; 
-
+            USB_MIDI_OUT_Service();
         }
-        */
     }
         
 }
@@ -1127,10 +1067,10 @@ const uint16_t sysexPointerMask = 1023;
 //data sent from computer
 void USB_callbackLocalMidiEvent(uint8 cable, uint8 *midiMsg) CYREENTRANT
 {
-    tempMIDI[0] = midiMsg[0];
-    tempMIDI[1] = midiMsg[1];
-    tempMIDI[2] = midiMsg[2];
-    tempMIDI[3] = midiMsg[3];
+    //tempMIDI[0] = midiMsg[0];
+    //tempMIDI[1] = midiMsg[1];
+    //tempMIDI[2] = midiMsg[2];
+    //tempMIDI[3] = midiMsg[3];
     //check that we got here
     if (receivingSysex)
     {
@@ -1496,7 +1436,7 @@ void handleNotes(int note, int velocity, int string)
             {
                 ignore = 1;   
             }
-            else if ((loudestSoundingNote >=25) && (velocity <= loudestSoundingNote -25))
+            //else if ((loudestSoundingNote >=25) && (velocity <= loudestSoundingNote -25))
             {
                 //ignore = 1;
             }
