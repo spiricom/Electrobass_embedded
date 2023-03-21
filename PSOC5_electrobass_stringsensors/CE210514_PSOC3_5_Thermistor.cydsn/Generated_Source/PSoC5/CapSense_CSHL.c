6594bdfc4cb1e47e09ce71160c7d4ac03506e4e4
spiricom <jeff@snyderphonics.com>
2023-03-21 13:32:47 -0400

    matching

--- PSOC5_electrobass_stringsensors/CE210514_PSOC3_5_Thermistor.cydsn/Generated_Source/PSoC5/CapSense_CSHL.c
+++ PSOC5_electrobass_stringsensors/CE210514_PSOC3_5_Thermistor.cydsn/Generated_Source/PSoC5/CapSense_CSHL.c
@@ -46,7 +46,7 @@ uint8 CapSense_sensorOnMask[(((CapSense_TOTAL_SENSOR_COUNT - 1u) / 8u) + 1u)];
 uint8 CapSense_lowBaselineResetCnt[CapSense_TOTAL_SENSOR_COUNT];
 
 uint16 CapSense_fingerThreshold[] = {
-    20u, 20u, 20u, 20u, 60u, 60u, 60u, 60u, 
+    16u, 16u, 16u, 16u, 60u, 60u, 60u, 60u, 
 };
 
 uint16 CapSense_noiseThreshold[] = {
